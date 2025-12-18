b1b3d29e1f8c5c02fc0b018dbcdefb602065759a
minha <luck7118@naver.com>
2025-12-18 21:21:55 +0900

    feat: 공통 버튼 컴포넌트 추가

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -78,6 +79,45 @@ function Home() {
       </div>
     </Container>
   );
+=======
+import BoxButton from '../../components/Button/BoxButton'
+import FloatingButton from '../../components/Button/FloatingButton'
+
+export default function Home() {
+  return (
+    <div>
+      {/* 1. 메인 페이지: 질문받기 (갈색 버튼, 화살표 없음) */}
+      <BoxButton color="brown" size="sizeMainBrown">
+        질문 받기
+      </BoxButton>
+      {/* 2. 메인 페이지: 질문하러 가기 (베이지 버튼, 화살표 있음) */}
+      <BoxButton color="beige" size="sizeMain" isArrow={true}>
+        질문하러 가기
+      </BoxButton>
+
+      {/* 3. 모달창: 질문 보내기 (갈색 버튼) */}
+      <BoxButton color="brown" size="sizeModal">
+        질문 보내기
+      </BoxButton>
+      {/* 4. 답변하기: 답변 완료 (갈색 버튼) */}
+      <BoxButton color="brown" size="sizeWide">
+        답변 완료
+      </BoxButton>
+      {/* 5. 답변하기: 수정 완료 (갈색 버튼) */}
+      <BoxButton color="brown" size="sizeWide">
+        수정 완료
+      </BoxButton>
+      {/* 6. 답변하기: 수정 완료 (갈색 버튼) */}
+      <BoxButton color="brown" size="sizeWide" disabled="true">
+        수정 완료
+      </BoxButton>
+      {/* 7. 질문 작성하기 버튼*/}
+      <FloatingButton type="write">질문 작성</FloatingButton>
+      {/* 8. 삭제하기 버튼*/}
+      <FloatingButton type="delete">삭제하기</FloatingButton>
+    </div>
+  )
+>>>>>>> f0d7b2e (feat: 공통 버튼 컴포넌트 추가)
 }
 
 export default Home;
