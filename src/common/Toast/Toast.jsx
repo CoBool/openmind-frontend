3281b3bf9bd720dc9fb8e5b80f737a41afbed033
ljh815 <qnfn1315@gmail.com>
2025-12-18 14:49:35 +0900

    공용 컴포넌트

---
+++ src/common/Toast/Toast.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import LinkImg from "../../assets/Icon/Link.svg"
+import KakaoImg from "../../assets/Icon/kakao.svg"
+import FacebookImg from "../../assets/Icon/facebook.svg"
+import styles from "./Toast.module.css"
+
+function Toast () {
+  const [toast, setToast] = useState(false);
+
+  const handleClick = () => {
+    setToast(true);
+
+    setTimeout(() => {
+      setToast(false)
+    }, 5000);
+  };
+
+  return (
+    <div className={styles.Toast}>
+      <img
+        src={LinkImg}
+        alt="링크 이미지"
+        onClick={handleClick}
+        className={styles.LinkImg}
+      />
+      <img src={KakaoImg} />
+      <img src={FacebookImg} />
+
+      {toast && (
+        <div className={styles.ToastText}>
+          <span className={styles.text}>URL이 복사되었습니다</span>
+        </div> 
+      )}
+      
+    </div>
+  )
+}
+
+export default Toast;
