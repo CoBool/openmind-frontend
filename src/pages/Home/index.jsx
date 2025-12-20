417124ed8302ae4672216efc27ae35b30eb46775
zzungju <zzungju07@gmail.com>
2025-12-20 12:00:33 +0900

    feat: add MainPage with layour, buttons and styling

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router'
+import MainPage from './MainPage'
 
 export default function Home() {
   return (
     <div>
       <Link to="/list">List</Link>
+      <MainPage />
     </div>
   )
 }
