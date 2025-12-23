258943091f4fd31ef9269e885cd4a1ceceda4518
범희정 <beomhijeong@gmail.com>
2025-12-24 07:10:38 +0900

    feat: answer page & edit menu components

--- src/pages/Post/PostAnswer.jsx
+++ src/pages/Post/PostAnswer.jsx
@@ -1,3 +1,9 @@
+<<<<<<< HEAD:src/pages/Post/PostAnswer.jsx
 export default function PostAnswer() {
   return <div>답변하기 페이지</div>;
 }
+=======
+import AnswerPage from './AnswerPage'
+
+export default AnswerPage
+>>>>>>> d362120 (feat: answer page & edit menu components):src/pages/Answer/index.jsx
