9045177f5606d42d5d43060ab66d3c2c55cf9f75
minha <luck7118@naver.com>
2025-12-23 18:32:57 +0900

    refactor: Button 컴포넌트를 순수 재사용 가능하도록 리팩토링

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,62 +1,36 @@
 import styles from './Button.module.css';
-import arrowBrown from '../../assets/Icon/arrowRightBrown.svg';
 
-const buttonVariants = {
-  type: {
-    box: styles.box,
-    floating: styles.floating,
-  },
-  variant: {
-    brown: styles.brown,
-    beige: styles.beige,
-  },
-
-  size: {
-    sm: styles.sm,
-    md: styles.md,
-  },
+const buttonSizes = {
+  sm: styles.sm,
+  md: styles.md,
+  mdFixed: styles.mdFixed,
 };
-
 /**
- * 공통 버튼 컴포넌트
- *
+ * 박스형 버튼 컴포넌트
  * @param {React.ReactNode} children - 버튼 안에 표시될 내용
- * @param {'box' | 'floating'} type - 버튼 타입 (기본: 'box')
- * @param {'brown' | 'beige'} variant - 버튼 색상 (기본: 'brown', box 타입만)
- * @param {'sm' | 'md'} size - 버튼 크기 (기본: 'md')
- * @param {boolean} disabled - 비활성화 여부 (기본: false)
- * @param {boolean} isArrow - 화살표 아이콘 표시 (기본: false, box 타입만)
- * @param {string} className - 외부에서 추가할 스타일 (width 제어 등)
- * @param {object} restProps - 나머지 props (onClick, onMouseEnter 등)
- *
+ * @param {'sm' | 'md'| 'mdFixed'} size - 버튼 크기 (기본: md)
+ * @param {boolean} disabled - 버튼 비활성화 여부 (기본: false)
+ * @param {string} className - 외부에서 추가할 스타일
  */
 function Button({
   children,
-  type = 'box',
-  variant = 'brown',
   size = 'md',
   disabled = false,
-  isArrow = false,
   className = '',
   ...restProps
 }) {
-  const typeClass = buttonVariants.type[type] ?? buttonVariants.type.box;
-  // 색상 클래스 선택
-  const variantClass =
-    type === 'box'
-      ? (buttonVariants.variant[variant] ?? buttonVariants.variant.brown)
-      : '';
+  const sizeClass = buttonSizes[size] ?? buttonSizes.md;
 
-  const sizeClass = buttonVariants.size[size] ?? buttonVariants.size.md;
-
-  const classNames = `${styles.button} ${typeClass} ${variantClass} ${sizeClass} ${className}`;
+  const classNames = `${styles.button} ${sizeClass} ${className}`;
 
   return (
-    <button className={classNames} disabled={disabled} {...restProps}>
+    // 클래스 적용: 기본 스타일 + 크기
+    <button
+      className={`${classNames}`}
+      disabled={disabled} // 비활성화 상태 전달
+      {...restProps}
+    >
       {children}
-      {isArrow && type === 'box' && (
-        <img src={arrowBrown} alt="화살표 이미지" />
-      )}
     </button>
   );
 }
