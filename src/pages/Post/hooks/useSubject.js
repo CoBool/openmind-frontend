31aff1a726dd14d16e6f09278f904ae193221d42
cobool <saokm5@gmail.com>
2025-12-29 02:24:47 +0900

    feat: 개별피드 에러 컴포넌트 간단한 디자인 추가 리액션 중복요청 차단 추가

--- src/pages/Post/hooks/useSubject.js
+++ src/pages/Post/hooks/useSubject.js
@@ -2,6 +2,11 @@ import { useState, useEffect } from 'react';
 
 import { getSubject } from '@/services/subjectsApi';
 
+/**
+ * @description 질문 대상을 조회하는 훅
+ * @param {number} subjectId - 질문 대상의 고유 식별자
+ * @returns {Object} 질문 대상, 에러, 로딩 상태
+ */
 export const useSubject = subjectId => {
   const [subject, setSubject] = useState(null);
   const [error, setError] = useState(null);
