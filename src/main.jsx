ca8b2eabc25df400f10bc2ebc4cac67f5a381228
minha <luck7118@naver.com>
2025-12-28 21:00:32 +0900

    fix: main.jsx 복구 (빌드 필수 파일)

---
+++ src/main.jsx
@@ -0,0 +1,54 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
+
+import { AuthProvider } from './provider/AuthPrivder.jsx';
+
+import Home from './pages/Home/index.jsx';
+import List from './pages/List/ListPage.jsx';
+import PostDetail from './pages/Post/PostDetail.jsx';
+import PostAnswer from './pages/Post/PostAnswer.jsx';
+import Layout from './components/layout/Layout.jsx';
+import './index.css';
+import { ToastProvider } from './contexts/Toast/ToastCopy.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: Layout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: 'list',
+        Component: List,
+      },
+      {
+        path: 'post',
+        children: [
+          {
+            path: ':subjectId',
+            Component: PostDetail,
+          },
+          {
+            path: ':subjectId/answer',
+            Component: PostAnswer,
+          },
+        ],
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AuthProvider>
+      <ToastProvider>
+        <RouterProvider router={router} />
+      </ToastProvider>
+    </AuthProvider>
+  </StrictMode>
+);
