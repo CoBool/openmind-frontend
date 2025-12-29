44083c1854cf9070d37ef362607c794beaecbe7d
zzungju <zzungju07@gmail.com>
2025-12-29 16:05:21 +0900

    refactor: apply Container Component to Main page and update Layout styles

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom';
+import styles from './Layout.module.css';
 import { ToastProvider } from '@/contexts/Toast/Toast.jsx';
 
 export default function Layout() {
