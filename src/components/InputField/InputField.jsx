8672d194160b20e5dc7c60f5b04034314fbc650a
zzungju <zzungju07@gmail.com>
2025-12-31 12:57:27 +0900

    style: add focus state handling to InputField

--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './InputField.module.css';
 
 function InputField({
@@ -9,9 +10,22 @@ function InputField({
   icon: Icon,
   ...inputProps
 }) {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => setIsFocused(true);
+
+  const handleBlur = () => setIsFocused(false);
+
   return (
-    <label htmlFor={id} className={styles.nameInputLabel}>
-      {Icon && <Icon className={`font-body3 ${styles.personIcon}`} />}
+    <label
+      htmlFor={id}
+      className={`${styles.nameInputLabel} ${isFocused ? styles.nameInputFocus : styles.nameInputBasic}`}
+    >
+      {Icon && (
+        <Icon
+          className={`font-body3 ${styles.personIcon} ${isFocused ? styles.colorFocus : styles.colorBasic}`}
+        />
+      )}
       <input
         id={id}
         type={type}
@@ -19,6 +33,8 @@ function InputField({
         className={`font-body3 ${styles.nameInput}`}
         value={value}
         onChange={onChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
         {...inputProps}
       />
     </label>
