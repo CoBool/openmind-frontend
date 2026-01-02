d9f7dcec090109ba7a4edae5631124c0756cf37c
cobool <saokm5@gmail.com>
2026-01-02 12:13:26 +0900

    chore: 병합

--- src/pages/Post/components/QuestionAnswerList/Moremenu.jsx
+++ src/pages/Post/components/QuestionAnswerList/Moremenu.jsx
@@ -37,7 +37,7 @@ function MoreMenu({
         disabled={disabled}
         onClick={handleToggle}
       >
-        <Icon name="more" className={styles.moreIcon} />
+        <Icon name="more" className={styles.moreIcon} width={24} height={24} />
       </button>
 
       {open && (
